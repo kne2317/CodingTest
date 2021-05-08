747fc43215f113da959a77ec92e9926ac8680478
kne2317 <skdms2317@gmail.com>
2021-05-09 00:20:31 +0900

    초기커밋

---
+++ p001.py
@@ -0,0 +1,17 @@
+import math
+
+def function_1(a):
+    if a >= 0:
+        return a
+    else:
+        return -a
+
+
+def function_2(a):
+    b=a*a
+    return math.sqrt(b)
+
+print(function_1(3))
+print(function_1(-2))
+print(function_2(3))
+print(function_2(-2))
