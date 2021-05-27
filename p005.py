eab3d9a0f1e040b820653ebc373ea3a5a67495a8
kne2317 <skdms2317@gmail.com>
2021-05-27 22:37:25 +0900

    Q5

---
+++ p005.py
@@ -0,0 +1,28 @@
+# a, b = input().split()
+# a=int(a)
+# b=int(b)
+# if a>b:
+#     print(">")
+# elif a==b:
+#     print("==")
+# else:
+#     print("<")
+
+#시험 점수를 입력받아 90 ~ 100점은 A,
+# 80 ~ 89점은 B,
+# 70 ~ 79점은 C,
+# 60 ~ 69점은 D,
+# 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+
+grade=input()
+grade=int(grade)
+if grade>=90:
+    print("A")
+elif grade>=80:
+    print("B")
+elif grade>=70:
+    print("C")
+elif grade>=60:
+    print("D")
+else:
+    print("F")
