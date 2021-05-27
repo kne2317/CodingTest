722b147bc143a3db9a73b281947adc7069b65a38
kne2317 <skdms2317@gmail.com>
2021-05-27 21:04:35 +0900

    commit

---
+++ p004.py
@@ -0,0 +1,32 @@
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
+
+# A, B = input().split()
+#
+# print(int(A)+int(B))
+# print(int(A)-int(B))
+# print(int(A)*int(B))
+# print(int(A)/int(B))
+# print(int(A)%int(B))
+
+
+# A, B, C = input().split()
+# A=int(A)
+# B=int(B)
+# C=int(C)
+# print((A+B)%C)
+# print(((A%C)+(B%C))%C)
+# print((A*B)%C)
+# print(((A%C)*(B%C))%C)
+
+
+
+A=input()
+B = input()
+print(int(A)*int(B[-1]))
+print(int(A)*int(B[-2:-1]))
+print(int(A)*int(B[-3:-2]))
+print(int(A)*int(B))
