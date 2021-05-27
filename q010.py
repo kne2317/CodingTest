81646505d9c0d772e7023526a69130c7849d62eb
kne2317 <skdms2317@gmail.com>
2021-05-28 01:03:40 +0900

    Q10-1

--- q010.py
+++ q010.py
@@ -11,3 +11,14 @@ def solution(name_list):
     return result
 name=["Tom","mike","Tom","jerry"]
 print(solution(name))
+
+
+
+def solution2(name_list):
+    for i in range(0, len(name_list)-1):
+        for j in range(i+1, len(name_list)):
+            print(name_list[i]+"-"+name_list[j])
+    return 0
+
+name2=["Tom","mike","jerry"]
+solution2(name2)
